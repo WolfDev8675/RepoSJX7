d2f05ebf8b327a0f4b60a757d0e2980932a56ae4
Bishal Biswas <55377241+WolfDev8675@users.noreply.github.com>
2020-10-20 21:01:50 +0530

    Update analysis1map.py

--- analysis1map.py
+++ analysis1map.py
@@ -1,38 +1,19 @@
-##** VisualStudio2019 code
 #!usr/bin/python
 #
 #   Analysis 1: 
-#       Mapper for isolating components of Revenue Aggregrate *
+#       Mapper for isolating components of Revenue Aggregate *
 #           by country for top 5 countries  **
-import sys 
-
-for line in sys.stdin:
-    row=line.strip()
-    components=row.split("\t")
-    if int(components[3])>= 0 or float(components[5])>= 0.0 :
-        print('%s,%d,%f'%(components[7],int(components[3]),float(components[5]))
-
-   
-
-
-# end of code 
-
-##** KomodoIDE code
-#!usr/bin/python
-#
-#   Analysis 1: 
-#       Mapper for isolating components of Revenue Aggregrate *
-#           by country for top 5 countries  **
-import sys 
-
-for line in sys.stdin:
-    row=line.strip()
-    components=row.split('\t')
-    if int(components[3])>= 0 or float(components[5])>= 0.0 :
-        print('%s,%d,%f'%(components[7],int(components[3]),float(components[5]))
-    else:
-       continue
-
-
-
+#  start of code
+import sys   
+for line in sys.stdin:      
+    row=line.strip()    # removeing white spaces
+    components=row.split("\t")      # separating individual components 
+    try:
+        if ( "" not in components or int(components[3])>= 0 or float(components[5])>= 0.0 ):   # filtration -> quality control 
+            print('%s,%d,%f'%(components[7],int(components[3]),float(components[5])))   # key: country ; values : quantity and unit price
+        else:
+            continue   #ignore condition failures -> quality control
+    except Exc:
+        continue    # ignore filtration failure -> quality control 
+    
 # end of code 
