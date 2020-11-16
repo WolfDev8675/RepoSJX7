eda9edf5a52a8a79eb83b01f2d49d6ff55536ade
Bishal Biswas <55377241+WolfDev8675@users.noreply.github.com>
2020-11-16 09:25:20 +0530

    Create analysis5red.py

---
+++ analysis5red.py
@@ -0,0 +1,33 @@
+#!usr/bin/python
+#
+#   Analysis 5: 
+#       Basket size distribution (Note: Basket size = number of items in a transaction) **
+#       (in this questions, we would like to know that, number of transactions by each basket size) ** 
+#       i.e. number of transactions with 3 size, number of transactions with 4 size etc. **
+#
+# ** start of code
+
+import sys
+from operator import itemgetter 
+data_list=[]        # local list 
+for line in sys.stdin:
+    data_line=line.strip()      #remove whitespaces
+    elements=data_line.split("\t")  #segregate components
+    data_list.append(elements)  # generate local list
+if len(data_list) is 0:
+    print(" Input Stream Error ... -> Reducer didn't receive any data ")
+    sys.exit(1)         # fatal error due to reducer not receiving any data
+data_dict={}  # storing all revenue aggregates
+for elem in data_list:
+    try:
+        # test code for input errors
+        if elem[0] not in data_dict:
+            # new found unique key :: invoice
+            data_dict[elem[0]]=int(elem[1])
+        else:
+            #  already available country key
+            data_dict[elem[0]] = data_dict[elem[0]]+int(elem[1])
+        
+    except Exception:
+        print (" Input Data Error ... -> Dictionary not generated or In-accessable data list ")
+        
