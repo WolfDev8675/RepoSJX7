c6c5a2df478eb4e1d28f3dd87211a62489e0c50f
Bishal Biswas <55377241+WolfDev8675@users.noreply.github.com>
2021-02-21 21:02:09 +0530

    Update and rename file9_xml.py to file_xml_rd_wr.py

--- file_xml_rd_wr.py
+++ file_xml_rd_wr.py
@@ -6,7 +6,7 @@
 # file source https://thingspeak.com/channels/129016
 
 #imports
-from file6_webScr import *
+from file_webScrRq import *
 import xml.etree.ElementTree as et
 import pandas as pd
 
@@ -19,22 +19,23 @@ filename_local='test_file.xml'
 xml_file=et.parse(accessFile(web_url,destination_local,filename_local))
 file_root=xml_file.getroot()
 
-# xml print 
+## xml print 
 #for gen0 in file_root:
 #    print(gen0.tag,'***',gen0.text)
 #    for gen1 in gen0:
 #        print(gen1.tag,">>>")
 #        for gen2 in gen1: 
 #            print(gen2.tag,"^^",gen2.text)
-
+# #results in xml_results1.txt
 
 
 # framing up with assimilation 
-data_colec=[]
+data_colec=[] # empty collectors
 data_dict={} 
+#navigation loops
 for gen0 in file_root:
-    print(gen0.tag,'***',gen0.text)
-    for gen1 in gen0:
+    print(gen0.tag,'***',gen0.text)  #primary informations
+    for gen1 in gen0:       # data specific 
         data_dict={} # level inialize
         for gen2 in gen1: 
             data_dict[gen2.tag]=gen2.text
@@ -46,3 +47,4 @@ print(" ".center(50,'*'))
 print(data_body)
 
 
+#end of code 
