3d9b1fb73999fcb7379359f1e3a3c9b8b10c2d5c
Bishal Biswas <55377241+WolfDev8675@users.noreply.github.com>
2021-02-25 23:24:14 +0530

    Update Workfile_task2.py

--- Task2/Workfile_task2.py
+++ Task2/Workfile_task2.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plotter
 
 # data file access
 rootpath=input("Please input the Path containg the Data ")
-if rootpath=="" : rootpath="Task2\\data"
+if rootpath=="" : rootpath="Task2\\data\\"
 file1name="car data.csv"
 file2name="car condition.csv"
 
