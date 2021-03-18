b96d52354f61d22bf23c942d9bfc5c6b10bee138
Bishal Biswas <55377241+WolfDev8675@users.noreply.github.com>
2021-03-18 20:17:54 +0530

    Tasks Creations Update 18032021-20:17:55 IST

---
+++ Task5/Workfile_task5.py
@@ -0,0 +1,12 @@
+#!usr/bin/python
+
+# Question ...... 
+#  Read car data.csv and car condition.csv. Merge them together
+#   (files already provided). Find out following from the data set.
+#   1. Apply dummy encoding.
+#   2. Do same functionality using one-hot encoding.
+#   3. Apply level encoding
+#..
+
+# Start of code 
+#..
---
+++ Task6/Workfile_task6.py
@@ -0,0 +1,13 @@
+#!usr/bin/python
+
+# Question .....
+# Read car data.csv and car condition.csv. Merge them together
+#   (files already provided). And complete following tasks.
+#   1. Plot bar chart, box-plot, violin plot, line plot.
+#   2. Build correlation matrix.
+#   3. Show distribution of data.
+#   4. Describe different statistics of data
+#..
+
+# Start of code 
+#..
