298bf55a8ef1408b00c579840756a4e7d31416a3
Bishal Biswas <55377241+WolfDev8675@users.noreply.github.com>
2021-04-28 23:20:56 +0530

    code correction

---
+++ PlaceEdits.py
@@ -0,0 +1,34 @@
+#!usr/bin/python
+
+""" Code for editing place details using GPS locations
+ for file 'place details.csv' """
+# code file: PlaceEdits.py
+# code author: BishalBiswas(https://github.com/WolfDev8675)
+
+#imports 
+import pandas as PD
+from geopy.geocoders import Nominatim as G_Loc
+
+
+# DataFrame
+placeDF=PD.read_csv('e:\Source\Repos\WolfDev8675\RepoSJX7\Data\place details.csv',encoding='latin-1')
+
+# operate 
+citydct={}
+citydct["Excepts"]=1
+
+idx=placeDF.index.to_list()
+GeoLoc=G_Loc(user_agent='Generic')
+for one_id in idx:
+  lat=str(placeDF.loc[one_id,'latitude'])
+  lon=str(placeDF.loc[one_id,'longitude'])
+  plc=GeoLoc.reverse(lat+","+lon).raw['address']
+  #print(plc['city'],"\t",plc['state'],"\t",plc['country']," ** ")
+  try:
+      if plc['city'] not in citydct: citydct[plc['city']]=1
+      else: citydct[plc['city']]+=1
+  except:
+      citydct["Excepts"]+=1
+      print(plc)
+
+print(citydct)
