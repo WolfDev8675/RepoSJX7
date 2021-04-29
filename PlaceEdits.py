1de062f335f267352c5a6a151c8f48d40719dba1
Bishal Biswas <55377241+WolfDev8675@users.noreply.github.com>
2021-04-29 19:38:30 +0530

    update to edited file

--- PlaceEdits.py
+++ PlaceEdits.py
@@ -14,23 +14,18 @@ from geopy.geocoders import Nominatim as G_Loc
 placeDF=PD.read_csv('e:\Source\Repos\WolfDev8675\RepoSJX7\Data\place details.csv',encoding='latin-1')
 
 # operate 
-citydct={}
-citydct["Excepts"]=1
-
 idx=placeDF.index.to_list()
 GeoLoc=G_Loc(user_agent='Generic')
 for one_id in idx:
   lat=str(placeDF.loc[one_id,'latitude'])
   lon=str(placeDF.loc[one_id,'longitude'])
   plc=GeoLoc.reverse(lat+","+lon).raw['address']
-  #print(plc['city'],"\t",plc['state'],"\t",plc['country']," ** ")
+  
   try:
-      if plc['city'] not in citydct: citydct[plc['city']]=1
-      else: citydct[plc['city']]+=1
       placeDF.loc[one_id,'city']=plc['city']
   except:
-      citydct["Excepts"]+=1
-      print(plc)
-
-print(citydct)
-placeDF.to_csv("e:\Source\Repos\WolfDev8675\RepoSJX7\EditedData\place details.csv") 
+      placeDF.loc[one_id,'city']=plc['county'] # since counties are found to be city equivalent matching state outlines perfectly in this case
+      # the classical case of city regions being shared between city municipality and another smaller neighbouring municipality
+  
+#push to secondary source 
+placeDF.to_csv("e:\Source\Repos\WolfDev8675\RepoSJX7\EditedData\place details.csv", encoding='latin-1', index=False)
