2f1f031c095e1133dbb9f389ed77efb2d583f338
Bishal Biswas <55377241+WolfDev8675@users.noreply.github.com>
2021-07-21 14:10:43 +0530

    Update 4

--- Task_GC_04/Task_Code04.py
+++ Task_GC_04/Task_Code04.py
@@ -9,6 +9,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier as DecTree
+from sklearn.preprocessing import OneHotEncoder as OHEnc
 
 #Import Data
 dbs=pd.read_csv("e:\Source\Repos\WolfDev8675\RepoSJX7\Task_GC_04\data.csv")
@@ -18,3 +19,22 @@ print(" Info of Data : \n",dbs.info())
 # Data distribution 
 X=dbs.iloc[:,-1:-1]
 y=dbs.iloc[:-1]
+
+# One Hot Encoding to fit data 
+ohec=OHEnc()
+X_enc=ohec.fit(X)
+Y_enc=ohec.fit(y)
+
+# Decisions 
+clfTree=DecTree()
+clfTree.fit(X_enc,Y_enc)
+
+# Tree Details
+clfTree.plot_tree()
+plt.show()
+
+
+
+#..
+# End of Code 
+#.. 
