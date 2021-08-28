6ec9160e85f7fc6f7ae2460128917cbc3f9dfc0e
Bishal Biswas <55377241+WolfDev8675@users.noreply.github.com>
2021-08-28 19:13:40 +0530

    update

--- API_read_yfinance.py
+++ API_read_yfinance.py
@@ -1,3 +1,14 @@
 #!usr/bin/python
 
 #API readers
+"""***Imports***"""
+
+import pandas as pd
+import yfinance as yf
+
+"""*Data access*"""
+
+BrentCrude=yf.Ticker("BZ=F")
+print(BrentCrude.info)
+DTX_BrentC=BrentCrude.history(period="5y")
+print(DTX_BrentC)
