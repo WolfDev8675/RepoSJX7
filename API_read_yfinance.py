111930404e5d5ccf950eb6ac5a7683f1b0d3e960
Bishal Biswas <55377241+WolfDev8675@users.noreply.github.com>
2021-08-29 16:57:02 +0530

    update

--- API_read_yfinance.py
+++ API_read_yfinance.py
@@ -5,6 +5,8 @@
 
 import pandas as pd
 import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plots
 
 """*Data access*"""
 
@@ -12,3 +14,9 @@ BrentCrude=yf.Ticker("BZ=F")
 print(BrentCrude.info)
 DTX_BrentC=BrentCrude.history(period="5y")
 print(DTX_BrentC)
+print(DTX_BrentC.info())
+
+x=DTX_BrentC.index;
+y=DTX_BrentC['Close'];
+plots.plot(x,y)
+plots.show()
