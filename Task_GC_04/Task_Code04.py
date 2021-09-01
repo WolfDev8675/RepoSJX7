9e046a6df789a8169f16b43b6442175cc8826b33
Bishal Biswas <b.biswas_94587@ieee.org>
2021-09-01 21:08:45 +0530

    Update Task_Code04.py

--- Task_GC_04/Task_Code04.py
+++ Task_GC_04/Task_Code04.py
@@ -9,28 +9,36 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier as DecTree
+from sklearn.tree import plot_tree
 from sklearn.preprocessing import OneHotEncoder as OHEnc
 
 #Import Data
-dbs=pd.read_csv("e:\Source\Repos\WolfDev8675\RepoSJX7\Task_GC_04\data.csv")
-print(" Head of the data \n",dbs.head(5))
-print(" Info of Data : \n",dbs.info())
+dbs=pd.read_csv("https://raw.githubusercontent.com/WolfDev8675/RepoSJX7/Assign5_miscellaneous/Task_GC_04/data.csv")
+print("\n\n Head of the data \n",dbs.head(5));
+print("\n\n Info of Data : \n");dbs.info();
+
+# Data conversion 
+dbs['Class']=dbs['Class'].astype('category') # Converting classifier field to categorical
 
 # Data distribution 
-X=dbs.iloc[:,-1:-1]
-y=dbs.iloc[:-1]
+print("\n\n Segregating  Data to the dependent and independent types ....")
+X=dbs[["Gender","Car Type","Shirt Size"]]  # Independent variables 
+y=dbs["Class"].cat.codes  # Dependent variables (categorical coded)
 
 # One Hot Encoding to fit data 
+print("\n Encoding ....")
 ohec=OHEnc()
 X_enc=ohec.fit_transform(X)
-Y_enc=ohec.fit(y)
+
 
 # Decisions 
+print("\n Creating the Tree model and training .... ")
 clfTree=DecTree()
-clfTree.fit(X_enc,Y_enc)
+clfTree.fit(X_enc,y)
 
 # Tree Details
-clfTree.plot_tree()
+print("\n\n Plot of the Finalized tree ")
+plot_tree(clfTree,class_names=['C0','C1'])
 plt.show()
 
 
