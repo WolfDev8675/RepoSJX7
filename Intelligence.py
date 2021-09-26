a14b861fedb63fb7f0983e40391db97b3b304a5f
Bishal Biswas <55377241+WolfDev8675@users.noreply.github.com>
2021-09-27 00:25:05 +0530

    update

--- Intelligence.py
+++ Intelligence.py
@@ -69,7 +69,7 @@ class Forecaster():
     def normal_split(self):
         """ Trains the forecasting model """ 
         X_train,X_test,y_train,y_test=train_test_split(self.XData,self.YData,test_size=0.3,random_state=0)
-        self.split_data={'Train':{'x':X_train,'y':y_train},'Test':{'x':X_test}}
+        self.split_data={'Train':{'x':X_train,'y':y_train},'Test':{'x':X_test,'y':y_test}}
         
 
     def crossval_KF_split(self, **kwargs):
@@ -82,30 +82,79 @@ class Forecaster():
             for train_index, test_index in CrsV.split(self.XData):
                 X_train,X_test=self.XData.iloc[train_index],self.XData.iloc[test_index]
                 y_train,y_test=self.YData.iloc[train_index],self.YData.iloc[test_index]
-            self.split_data={'Train':{'x':X_train,'y':y_train},'Test':{'x':X_test}}
+            self.split_data={'Train':{'x':X_train,'y':y_train},'Test':{'x':X_test,'y':y_test}}
         else:
             raise ValueError(" No X,Y definition found ")
         
     def train(self):
         """ Trains the model on given data """
         self.model.fit(self.split_data['Train']['x'],self.split_data['Train']['y'])
+    
+    def train_full(self):
+        """ Trains on the whole data """
+        self.model.fit(self.XData,self.YData)
 
     def plotMetrics(self,data,title="Model: unspecified"):
         """ Plots variation of the trained data """
         XData=data[self.predicts]
         YData=data[self.infers]
-        anotStr='\n'.join((r"Mean Absolute Error: %f"%metrics.mean_absolute_error(YData,self.model.predict(XData)),
-        r" Mean Squared Error:  %f"%metrics.mean_squared_error(YData,self.model.predict(XData)),
-        r" Root Mean Squared Error:  %f"%np.sqrt(metrics.mean_squared_error(YData,self.model.predict(XData)))))
-        predLR=self.model.predict(data[['Open','High','Low']])
+        prediction=self.model.predict(XData)
+        anotStr='\n'.join((r" Mean Absolute Error : %f".ljust(45,' ')%metrics.mean_absolute_error(YData,prediction),
+        r" Mean Squared Error : %f".ljust(45,' ')%metrics.mean_squared_error(YData,prediction),
+        r" Root Mean Squared Error : %f".ljust(45,' ')%np.sqrt(metrics.mean_squared_error(YData,prediction)),
+        r" R{:} : %f".format('\xb2').ljust(45,' ')%metrics.r2_score(YData,prediction),
+        r" Adjusted R{:} : %f".format('\xb2').ljust(45,' ')%(1-(1-metrics.r2_score(YData,prediction))*(len(YData)-1)/(len(YData)-XData.shape[1]-1)),
+        r" Mean Absolute Percentage Error : %f".ljust(45,' ')%metrics.mean_absolute_percentage_error(YData,prediction)))
         Fig=plots.figure(figsize=(15,7.5));ax=Fig.add_subplot(111);
         plots.plot(data.index,data[[self.infers]])
-        plots.plot_date(data.index,predLR,'g.-')
-        plots.text(0.12,0.95,anotStr,horizontalalignment='center',
+        plots.plot_date(data.index,prediction,'g.-')
+        plots.text(0.147,0.915,anotStr,horizontalalignment='center',
             verticalalignment='center', transform=ax.transAxes)
         plots.title(title)
         plots.show()
 
+    def cost_function(self):
+        """ Calculate the Cost incurred of Error """
+        pass
+
     def grad_decent(self):
         """ Use Gradient Decent method to purify model """
         pass
+
+    def ensemble_grading(self):
+        """ Operate ensemble gradation on data to improve model """
+        pass
+
+    def boost(self):
+        """ Variable boosting for model improvement """
+        pass
+
+    def regression_report(self):
+        """ Regression report collection """
+        y_true,y_predic=self.split_data['Test']['y'],self.model.predict(self.split_data['Test']['x'])
+        errors=y_true-y_predic
+        percentiles=[5,25,50,75,95]
+        perc_vals=np.percentile(errors,percentiles)
+
+        reports=[
+            ('Mean Absolute Error',metrics.mean_absolute_error(y_true,y_predic)),
+            ('Mean Squared Error',metrics.mean_squared_error(y_true,y_predic)),
+            ('Root Mean Squared Error',metrics.mean_squared_error(y_true,y_predic)),
+            ('Mean Squared Log Error',metrics.mean_squared_log_error(y_true,y_predic)),
+            ('Median Absolute Error',metrics.median_absolute_error(y_true,y_predic)),
+            (' R{:} '.format('\xb2'),metrics.r2_score(y_true,y_predic)),
+            (' Adjusted R{:} : '.format('\xb2'),(1-(1-metrics.r2_score(y_true,y_predic))*(len(y_true)-1)/(len(y_true)-self.split_data['Test']['x'].shape[1]-1))),
+            ('Mean Poisson Deviance',metrics.mean_poisson_deviance(y_true,y_predic)),
+            ('Mean Gamma Deviance',metrics.mean_gamma_deviance(y_true,y_predic)),
+            ('Max Error',metrics.max_error(y_true,y_predic)),
+            ('Explained Variance',metrics.explained_variance_score(y_true,y_predic)),
+            ('Mean Absolute Percentage Error',metrics.mean_absolute_percentage_error(y_true,y_predic))]
+
+        print("".center(70,'_'))
+        print(' Regression Metrics Report ')
+        for metric,value in reports:
+            print(f'{metric:>35s}: {value: >15.6f}')
+        print("".center(70,'_'))
+        print(' Percentiles ')
+        for per,vals in zip(percentiles,perc_vals):
+            print(f'{per:>15d}: {vals: >15.6f}')
--- runtime.py
+++ runtime.py
@@ -27,8 +27,17 @@ model_TS=seasonal_decompose(dHs[['Close']],model='additive',period=30)
 model_TS.plot()
 plots.show() 
 
-#boxplot
+#boxplot @visuals 
 plots.boxplot(dHs[['Open','High','Low','Close']],labels=['Open','High','Low','Close']);plots.show()
+ 
+# EDA 
+# yearwise trend open vs close and high alternations check @visuals
+#import seaborn 
+#seaborn.pairplot(dHs[['Open','High','Low','Close']]);plots.show()
+## per year open and close line curves 
+
+
+# 
 
 #Model Creation and study
 models_name={'LR':"Linear Regression",
@@ -42,14 +51,31 @@ models_mods_norm={'LR':Forecaster(model=LinReg()),
 models_mods_CVKF=deepcopy(models_mods_norm);
 
 # Train and Test 
-for mod_key in models_mods_norm:
-    models_mods_norm[mod_key].pushData(data=dHs,predicts=['Open','High','Low'],infers="Close")
-    models_mods_norm[mod_key].normal_split()
-    models_mods_norm[mod_key].train()
-    models_mods_norm[mod_key].plotMetrics(data=dhs2,title="Model: "+models_name[mod_key])
+#for mod_key in models_mods_norm:
+#    models_mods_norm[mod_key].pushData(data=dHs,predicts=['Open','High','Low'],infers="Close")
+#    models_mods_norm[mod_key].normal_split()
+#    models_mods_norm[mod_key].train()
+#    models_mods_norm[mod_key].plotMetrics(data=dhs2,title="Model: "+models_name[mod_key])
 
 for mod_key in models_mods_CVKF:
     models_mods_CVKF[mod_key].pushData(data=dHs,predicts=['Open','High','Low'],infers="Close")
     models_mods_CVKF[mod_key].crossval_KF_split(n_splits=5,random_state=None,shuffle=False)
     models_mods_CVKF[mod_key].train()
     models_mods_CVKF[mod_key].plotMetrics(data=dhs2,title="Model: "+models_name[mod_key]+" Cross Validated ")
+
+#Report on models 
+for mod_key in models_mods_CVKF:
+    if mod_key is not 'LR':
+        print(models_name[mod_key])
+        models_mods_CVKF[mod_key].regression_report()
+    else:
+        print(models_name[mod_key])
+        models_mods_CVKF[mod_key].grad_decent()
+        models_mods_CVKF[mod_key].ensemble_grading()
+        models_mods_CVKF[mod_key].boost()
+        models_mods_CVKF[mod_key].regression_report()
+
+# Report on train vs test vs predicted
+#  predicted vs test plot report  @visuals 
+#  kind of residuals plots  
+# Reasons and conclusions + post decisions and future advs 
